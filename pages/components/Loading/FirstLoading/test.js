d5e956d5f2ce4b3dc3dd16bd7a15adc0cee45e4e
Marcos Vaz <marcosvaz.me@gmail.com>
2020-08-17 19:41:19 -0300

    latest

--- pages/components/Loading/FirstLoading/test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
-import FirstLoading from './index';
-
-it('Deve renderizar um "loading" com uma mensagem', () => {
-  const { getByText } = render(<FirstLoading />);
-  expect(getByText('Olá! Seja bem vindo(a)!')).toBeInTheDocument();
-});
