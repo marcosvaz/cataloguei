a50a39d67708a4b59d565a7752576d59210f3763
Marcos Vaz <marcosvaz.me@gmail.com>
2020-08-17 17:10:25 -0300

    deleted Top component

--- pages/components/Top/test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
-import Top from './index';
-
-it('Deve renderizar um "botão" de topo', () => {
-  const { getByText } = render(<Top />);
-  expect(getByText('Topo')).toBeInTheDocument();
-});
